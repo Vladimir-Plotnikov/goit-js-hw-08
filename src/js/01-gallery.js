2110f736a4a470fcfd079e7094b8b725bcd2235d
Volodymyr <vovdata@gmail.com>
2022-11-06 10:41:08 +0100

    Update 01-gallery.js
    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery')
+const gMarkUp = createMarkUp(galleryItems)
+
+galleryList.insertAdjacentHTML('beforeend', gMarkUp)
+galleryList.addEventListener('click', onPictureClick);
+
+function createMarkUp(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<ul class="gallery"> 
+        <li class = "some"> <a class="gallery__item" href="${original}">
+        <img class="gallery__image some" src="${preview}" alt="${description}"/></a>
+        </li>
+        </ul>`;
+    })
+        .join('')
+}
+function onPictureClick(evt) {
+    evt.preventDefault();
+    const picSpace = evt.target.classList.contains('gallery__image')
+    if (!picSpace) {
+     return;   
+    } 
+    new SimpleLightbox('.some a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+}   
+
 console.log(galleryItems);
